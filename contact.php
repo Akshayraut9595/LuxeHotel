9b462f8b22c7381b9574b74bc8dfeeee4baa137d
Akshayraut9595 <121293999+Akshayraut9595@users.noreply.github.com>
2023-09-09 22:55:09 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TJ Hotel - Facilities</title>
+    
+    <?php require('inc/links.php')?>
+    <style>
+    </style>
+</head>
+<body class="bg-light">
+    <?php require('inc/header.php')?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-fonts text-center">CONTACT US</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Fugit ipsum odit, ad ipsam sapiente hic sed iusto atque cum repellendus!</p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-6 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4">
+                    <iframe class="w-100 rounded mb-4" height="320px"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230660.99610611217!2d81.66149884934819!3d25.4021973904255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398534c9b20bd49f%3A0xa2237856ad4041a!2sPrayagraj%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1690655973875!5m2!1sen!2sin"style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <h5>Address</h5>
+                    <a href="https://goo.gl/maps/qF2pxyWNzLG7Pjcy7" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+                        <i class="bi bi-geo-alt-fill"></i> XYZ,Prayagraj,Utter Pradesh</a>
+                    <h5 class="mt-4">Call us</h5>
+                    <a href="tel: +919503896316" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill me-1"></i>+919503896316
+                    </a>
+                    <br>
+                    <a href="tel: +919503896316" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill me-1"></i>+919503896316
+                    </a>
+
+                    <h5 class="mt-4">Email</h5>
+                    <a href="mailto: ask.akshayraut8080@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-envelope-fill"></i> ask.akshayraut8080@gmail.com
+                    </a>
+
+                    <h5 class="mt-4">Follow us</h5>
+                    <a href="#" class="d-inline-block text-dark fs-5 me-2">
+                        <i class="bi bi-twitter me-1"></i>
+                    </a>
+                   
+                    <a href="#" class="d-inline-block text-dark fs-5 me-2">
+                        <i class="bi bi-facebook me-1"></i>
+                    </a>
+                   
+                    <a href="#" class="d-inline-block text-dark fs-5">
+                        <i class="bi bi-instagram me-1"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 px-4">
+                <div class="bg-white rounded shadow p-4">
+                    <form action="">
+                        <h5>Send a message</h5>
+
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Name</label>
+                            <input type="text" class="form-control shadow-none">
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Email</label>
+                            <input type="email" class="form-control shadow-none">
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Subject</label>
+                            <input type="text" class="form-control shadow-none">
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label" style="font-weight: 500;">Message</label>
+                            <textarea class="form-control shadow-none"rows="5" style="resize: none;"></textarea>
+                        </div>
+                        <button type="submit" class="btn text-white custom-bg mt-3">SEND</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require('inc/footer.php')?>
+</body>
+</html>
